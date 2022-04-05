afabaf6c709d84a282e1edad5f14a9b5c5bdabd1
yuto14prog <yuto14prog@gmail.com>
2022-04-05 13:22:37 +0900

    Row設置

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,15 @@
+import { requests } from './api/request';
 import './App.css';
+import { Row } from './components/Row/Row';
 
 function App() {
   return (
     <div className="App">
-
+      <Row title="NETFLIX ORIGUINALS" fetchUrl={requests.feachNetflixOriginals} isLargeRow />
+      <Row title="Top Rated" fetchUrl={requests.feactTopRated} />
+      <Row title="Comedy Movies" fetchUrl={requests.feactComedyMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.feactRomanceMovies} />
+      <Row title="DOcumentaries" fetchUrl={requests.feactDocumentMovies} />
     </div>
   );
 }
