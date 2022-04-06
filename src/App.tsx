e527bd611d33ba89a5cd0f3217f55f9ade6b60c1
yuto14prog <yuto14prog@gmail.com>
2022-04-06 11:36:39 +0900

    Nav設置

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import { requests } from './api/request';
 import './App.css';
 import { Banner } from './components/Banner/Banner';
+import { Nav } from './components/Nav/Nav';
 import { Row } from './components/Row/Row';
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Banner />
       <Row title="NETFLIX ORIGUINALS" fetchUrl={requests.fetchNetflixOriginals} isLargeRow />
       <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
